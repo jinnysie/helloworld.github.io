4237655a4688b6d27168764d36a6f3896b333237
Jinny <110340368+jinnysie@users.noreply.github.com>
2024-10-27 15:31:16 +0700

    Add files via upload
    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentIndex = 0;
+const images = ["images1.png", "images2.jpg", "images3.jpg"]; // Ganti dengan nama file gambarmu
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("slider-img").src = images[currentIndex];
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    document.getElementById("slider-img").src = images[currentIndex];
+}
+
+function toggleFunction() {
+    const toggle = document.getElementById("toggle");
+    const status = document.getElementById("toggle-status");
+    status.textContent = toggle.checked ? "On" : "Off";
+}
